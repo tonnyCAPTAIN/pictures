429afe5ae2cb1d4eca3df60763ef672ecddf7126
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2024-09-10 01:31:30 +0300

    web crawler

--- a_posts/views.py
+++ a_posts/views.py
@@ -3,6 +3,8 @@ from .models import *
 from django.forms import ModelForm
 from django import forms
 # from .forms import *
+from bs4 import BeautifulSoup
+import requests
 
 
 def home_view(request):
@@ -13,12 +15,13 @@ def home_view(request):
 class PostCreateForm(ModelForm):
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['url', 'body']
         labels = {
             'body': 'Caption',
         }
         widgets = {
             'body' : forms.Textarea(attrs={'rows':3, 'placeholder': 'Add a Caption...', 'class': 'font1 text-4xl'}),
+            'url' : forms.Textarea(attrs={'placeholder': 'Add url ...'}),
         }
 
 def post_create_view(request):
@@ -27,6 +30,26 @@ def post_create_view(request):
     if request.method == 'POST':
         form = PostCreateForm(request.POST)
         if form.is_valid():
-            form.save()
+            post= form.save(commit=False)
+            
+            website = requests.get(form.data['url'])
+            
+            sourcecode = BeautifulSoup(website.text, 'html.parser')
+            
+            find_image = sourcecode.select('meta[content^="https://live.staticflickr.com/"]')
+            
+            image = find_image[0]['content']
+            post.image = image
+            
+            find_title = sourcecode.select('h1.photo-title')
+            title = find_title[0].text.strip()
+            
+            post.title = title
+            
+            find_artist = sourcecode.select('a.owner-name')
+            artist = find_artist[0].text.strip()
+            post.artist = artist
+            
+            post.save()
             return redirect('home')
     return render(request, 'a_posts/post_create.html', {'form' : form})
