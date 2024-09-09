1383e95af7804cfee27674cafa3dea113a7e9a4c
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2024-09-10 00:12:45 +0300

    post

--- a_posts/views.py
+++ a_posts/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from django.forms import ModelForm
+from django import forms
+# from .forms import *
+
 
 def home_view(request):
+    posts = Post.objects.all()
+    return render(request, 'a_posts/home.html', {'posts' : posts})
+
+
+class PostCreateForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
+        labels = {
+            'body': 'Caption',
+        }
+        widgets = {
+            'body' : forms.Textarea(attrs={'rows':3, 'placeholder': 'Add a Caption...', 'class': 'font1 text-4xl'}),
+        }
+
+def post_create_view(request):
+    form = PostCreateForm()
     
-    return render(request, 'a_posts/home.html')
+    if request.method == 'POST':
+        form = PostCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request, 'a_posts/post_create.html', {'form' : form})
