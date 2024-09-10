905cb527aaf65cd1f8ba3928b5df8574882bd9ca
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2024-09-11 02:24:13 +0300

    user profile

--- a_core/settings.py
+++ a_core/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'django_cleanup.apps.CleanupConfig',
     'a_posts',
+    'a_users',
 ]
 
 SITE_ID = 1
---
+++ a_users/forms.py
@@ -0,0 +1,16 @@
+from .models import *
+from django.forms import ModelForm
+from django import forms
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+        labels = {
+            'realname' : 'Name'
+        }
+        widgets= {
+            'image': forms.FileInput(),
+            'bio' : forms.Textarea(attrs={'rows':3})
+        }
