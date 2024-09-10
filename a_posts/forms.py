4d7678515be7590ccd85737e0e935535c6f9bfba
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2024-09-10 16:12:26 +0300

    category

--- a_posts/forms.py
+++ a_posts/forms.py
@@ -6,24 +6,28 @@ from .models import *
 class PostCreateForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['url', 'body']
+        fields = ['url', 'body', 'tags']
         labels = {
             'body': 'Caption',
+            'tags': 'Category'
         }
         widgets = {
             'body' : forms.Textarea(attrs={'rows':3, 'placeholder': 'Add a Caption...', 'class': 'font1 text-4xl'}),
             'url' : forms.Textarea(attrs={'placeholder': 'Add url ...'}),
+            'tags': forms.CheckboxSelectMultiple(),
         }
         
 
 class PostEditForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['body',]
+        fields = ['body', 'tags']
         labels = {
             'body' :'',
+            'tags': 'Category'
         }
         widgets = {
-            'body' : forms.Textarea(attrs={'row': 3, 'class': 'font1 text-4xl'})
+            'body' : forms.Textarea(attrs={'row': 3, 'class': 'font1 text-4xl'}),
+            'tags': forms.CheckboxSelectMultiple(),
         }
         
--- a_posts/views.py
+++ a_posts/views.py
@@ -6,9 +6,22 @@ import requests
 from django.contrib import messages
 
 
-def home_view(request):
-    posts = Post.objects.all()
-    return render(request, 'a_posts/home.html', {'posts' : posts})
+def home_view(request, tag=None):
+    if tag:
+        posts = Post.objects.filter(tags__slug=tag)
+        tag = get_object_or_404(Tag, slug=tag)
+    else:
+        posts = Post.objects.all()
+    
+    categories = Tag.objects.all()
+    context = {
+        'categories' : categories,
+        'posts' : posts,
+        'tag' : tag
+    }
+        
+    return render(request, 'a_posts/home.html', context)
+
 
 
 
@@ -39,6 +52,7 @@ def post_create_view(request):
             post.artist = artist
             
             post.save()
+            form.save_m2m()
             return redirect('home')
     return render(request, 'a_posts/post_create.html', {'form' : form})
 
