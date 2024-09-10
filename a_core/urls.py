8a8e0d7890137531c58ac17753e8e55febce07a8
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2024-09-10 17:37:49 +0300

    images

--- a_core/urls.py
+++ a_core/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from a_posts.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,5 @@ urlpatterns = [
     path('post/<pk>/', post_page_view, name='post'),
    
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
